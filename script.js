7ad2c58491896940187ed0148dae7e0c368e430b
Matheus <matheusestevao0309@gmail.com>
2024-05-04 20:23:00 -0300

    Adicionando as imagens do figma no slide show

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // SlideShow Automático
-let imagens=['/img/pitbull.png','/img/pngwing.com.png'];
+let imagens=['/img/anuncicelular2.png','/img/anunciocelular1.png','/img/anunciocelular3.png','/img/anunciocelular4.png'];
 let index = 0;
 let time = 3000;
 
