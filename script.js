27875ef27cff4dd0d9abff103a3513882432dc75
Ian <ianmonteiromoreira@gmail.com>
2024-05-05 19:21:19 -0300

    Corrigindo as fotos do SlideShow

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // SlideShow Automático
-let imagens=['/img/anunciopc1.png','/img/anunciopc2.png','/img/anunciopc3.png','/img/anunciopc4.png'];
+let imagens=['./img/anunciopc1.png','./img/anunciopc2.png','./img/anunciopc3.png','./img/anunciopc4.png'];
 let index = 0;
 let time = 3000;
 
