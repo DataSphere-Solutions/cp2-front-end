4730ea262b7271e172cb35e3046070e72a29df03
igor brunelli ralo <igorbrunelli61@gmail.com>
2024-05-04 22:54:46 -0300

    mudando o slide show

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // SlideShow Automático
-let imagens=['/img/anuncicelular2.png','/img/anunciocelular1.png','/img/anunciocelular3.png','/img/anunciocelular4.png'];
+let imagens=['/img/anunciopc1.png','/img/anunciopc2.png','/img/anunciopc3.png','/img/anunciopc4.png'];
 let index = 0;
 let time = 3000;
 
